c048edb1ab3c8f98c5288869fbd49cc87667b941
Jon A <jonathanfaranda@gmail.com>
2025-06-24 17:13:32 -0500

    Install Google Tag Manager (GTM-NGD7MTC9) via next/script and noscript fallback in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from '@/app/(components)/shared/Header';
 import Footer from '@/app/(components)/shared/Footer';
 import AnalyticsWrapper from '@/app/(components)/AnalyticsWrapper';
 import { Analytics } from "@vercel/analytics/next";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,8 +27,34 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://api.supabase.com" />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NGD7MTC9');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NGD7MTC9"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="gtm"
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main className="pt-16">
           {children}
